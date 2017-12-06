e15f91fd4971f44a1f00bd070b7400dd8b841765
Artthik Sellathurai <artthik.sellathurai@students.unibe.ch>
2017-12-06 14:18:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function doToElementsInArray(array, callback){
   array.forEach(callback);
 }
 
-function changeCompletely(element) {
-  element = "sdlfkjgnsfdgjh";
-  console.log(element);
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
